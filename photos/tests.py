927d2228e2a5fc13e5b17623741c026914df67a0
FeverCode <giddylancs.gl@gmail.com>
2022-05-28 18:30:37 +0300

    Created test for category

--- photos/tests.py
+++ photos/tests.py
@@ -24,3 +24,25 @@ class LocationTestClass(TestCase):
         self.new_location.delete_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) == 0)
+        
+class CategoryTestCase(TestCase):
+    # Set up method
+    def setUp(self):
+        self.new_category = Category(category_name = 'Travel')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_category,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.new_category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.new_category.save_category()
+        self.new_category.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
