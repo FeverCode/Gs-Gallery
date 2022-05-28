0d7ef266886a4b46a22b8a742cdbead5ea0251af
FeverCode <giddylancs.gl@gmail.com>
2022-05-28 18:28:59 +0300

    Created test for location

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.new_location = Location(location_name = 'Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_location,Location))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.new_location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
