6bb3e7042fe69b7833177efa9d8513a8165471f8
FeverCode <giddylancs.gl@gmail.com>
2022-05-29 11:40:42 +0300

    Updated image display with modal view

--- gallery/urls.py
+++ gallery/urls.py
@@ -19,6 +19,6 @@ from photos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('',include("photos.urls")),
     path('showlist', views.showlist, name='showlist'),
 ]
