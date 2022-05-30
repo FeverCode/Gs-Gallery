c4a9daac0d9423844501e915e7617967e5c3f34b
FeverCode <giddylancs.gl@gmail.com>
2022-05-30 08:36:22 +0300

    Heroku deployment debug

--- gallery/settings.py
+++ gallery/settings.py
@@ -23,18 +23,6 @@ from decouple import config, Csv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*lhcd0x@++&sp1)9_42pig)!(-1q2fgvaxf2&b#4q1jc)27@%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
